a7ab806268260384391a792b1e0ffbf0b28bd36c
taitesen <iamhmunhlei@gmail.com>
2024-04-02 02:45:43 +0530

    adjust the control flow of the statement in UltimatePyramid

--- UlimatePyramid/main.c
+++ UlimatePyramid/main.c
@@ -5,50 +5,53 @@ void make_FPyramid(int input);
 void make_LPyramid(int input);
 void make_RPyramid(int input);
 void make_pyramid(int input);
+void show_error();
 
 int main() {
   int choice = 0;
   int input = 0;
   int number_of_lines = 0;
+  printf("----------------\n");
   printf("1.\n");
   make_7Pyramid(4);
-  printf("\n");
+  printf("\n----------------\n");
   printf("2.\n");
   make_FPyramid(4);
-  printf("\n");
+  printf("\n----------------\n");
   printf("3.\n");
   make_LPyramid(4);
-  printf("\n");
+  printf("\n----------------\n");
   printf("4.\n");
   make_RPyramid(4);
-  printf("\n");
+  printf("\n----------------\n");
   printf("5.\n");
   make_pyramid(4);
-  printf("\n");
-  printf("Which type of Pyramid do you want?\n");
+  printf("\n----------------\n");
+  printf("Which type of Pyramid do you want: ");
   scanf("%d", &choice);
-  printf("How many lines do you want: ");
-  scanf("%d", &number_of_lines);
-  switch (choice) {
-  case 1:
-    make_7Pyramid(number_of_lines);
-    break;
-  case 2:
-    make_FPyramid(number_of_lines);
-    break;
-  case 3:
-    make_LPyramid(number_of_lines);
-    break;
-  case 4:
-    make_RPyramid(number_of_lines);
-    break;
-  case 5:
-    make_pyramid(number_of_lines);
-    break;
-  default:
-    printf("-----------------------\n");
-    printf("|       ERROR         |\n");
-    printf("-----------------------\n");
+  if (choice > 5) {
+    show_error();
+    return 0;
+  } else {
+    printf("How many lines do you want: ");
+    scanf("%d", &number_of_lines);
+    switch (choice) {
+    case 1:
+      make_7Pyramid(number_of_lines);
+      break;
+    case 2:
+      make_FPyramid(number_of_lines);
+      break;
+    case 3:
+      make_LPyramid(number_of_lines);
+      break;
+    case 4:
+      make_RPyramid(number_of_lines);
+      break;
+    case 5:
+      make_pyramid(number_of_lines);
+      break;
+    }
   }
 
   return 0;
@@ -109,4 +112,10 @@ void make_pyramid(int input) {
     }
     printf("\n");
   }
+}
+
+void show_error() {
+  printf("-----------------\n");
+  printf("| Invalid Input |\n");
+  printf("-----------------\n");
 }
