1e53550f74feec9c02583c051f4b7b802a807e54
taitesen <iamhmunhlei@gmail.com>
2024-04-13 19:15:57 +0530

    adding PerfectSquares4Digit

---
+++ PerfectSquares4Digit/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+void generate4DigitPerfectSquares() {
+  for (int i = 4; i < 10;
+       i++) { // since the first two squared could only be between 4^2 to 9^2
+    int squaredFirstDigit = (i * i);
+    if ((squaredFirstDigit > 9) &&
+        (squaredFirstDigit < 100)) { // it could only be double digit
+      for (int j = 4; j < 10; j++) { // same as the first two squared number
+        int squaredSecondDigit = (j * j);
+        if ((squaredSecondDigit > 9) &&
+            (squaredSecondDigit < 100)) { // it could only be double digit
+          int fourDigitNumber = (squaredFirstDigit * 100) + squaredSecondDigit;
+          for (int k = 31; k < 99; k++) { // range for 4-digit numbers
+            int range_helper = (k * k);
+            if (fourDigitNumber == range_helper) {
+              printf("Ans: %d\n", fourDigitNumber);
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+int main() {
+  generate4DigitPerfectSquares();
+  return 0;
+}
