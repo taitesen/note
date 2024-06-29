a296464f774faa3cdc78bcb413c60f704b093f33
taitesen <iamhmunhlei@gmail.com>
2024-06-29 09:15:23 +0530

    modified PerfectSquares2Digit

--- PerfectSquares4Digit/main.c
+++ PerfectSquares4Digit/main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 void generatePerfectSquares() {
-  for (int i = 4; i < 10;
-       i++) { // since the first two squared could only be between 4^2 to 9^2
+  // since the first two squared could only be between 4^2 to 9^2
+  for (int i = 4; i < 10; i++) {
     int squaredFirstDigit = (i * i);
-    if ((squaredFirstDigit > 9) &&
-        (squaredFirstDigit < 100)) { // it could only be double digit
-      for (int j = 4; j < 10; j++) { // same as the first two squared number
+    // it could only be double digit
+    if ((squaredFirstDigit > 9) && (squaredFirstDigit < 100)) {
+      // same as the first two squared number
+      for (int j = 4; j < 10; j++) {
         int squaredSecondDigit = (j * j);
-        if ((squaredSecondDigit > 9) &&
-            (squaredSecondDigit < 100)) { // it could only be double digit
+        // it could only be double digit
+        if ((squaredSecondDigit > 9) && (squaredSecondDigit < 100)) {
           int fourDigitNumber = (squaredFirstDigit * 100) + squaredSecondDigit;
           for (int k = 31; k < 99; k++) { // range for 4-digit numbers
             int range_helper = (k * k);
